b727fad13852fddf161eb896d00d45ca6087fb0d
Jose <josebolorinos@gmail.com>
2017-10-22 20:05:30 -0700

    Added option to output csv of concatenated file to cat_dfs.py

--- cat_dfs.py
+++ cat_dfs.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def cat_dfs(ip_paths, idx_var = None):
+def cat_dfs(ip_paths, idx_var = None, output = False):
 	
 	concat_dlist = []
 	for ip_path in ip_paths:
@@ -13,5 +13,9 @@ def cat_dfs(ip_paths, idx_var = None):
 	# Sort by index (if given)
 	if idx_var:
 		concat_data.sort_values(idx_var, inplace = True)
+
+	if output:
+		ip_dir = os.path.dirname(ip_paths[0])
+		concat_data.to_csv(ip_dir, index = False, encoding = 'utf-8')
 	
 	return concat_data
