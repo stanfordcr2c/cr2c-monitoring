4452a1e8c1f469a25282376b63e4b254728568ce
jbolorinos <josebolorinos@gmail.com>
2017-10-29 21:01:25 -0700

    Modified get_reactor_plots.py to handle new hmi sql database

--- get_reactor_plots.py
+++ get_reactor_plots.py
@@ -15,17 +15,13 @@ import warnings
 import os
 import math
 import sys
-import hmi_data_agg as hmi
-import cat_dfs as cat
+import get_hmi_data as hmi
 
 def get_tmp_plots(
 	start_dt_str,
 	end_dt_str,
 	detailed = False,
-	outdir = None,
-	hmi_path = None, 
-	feeding_paths = None, 
-	tmp_paths = None
+	outdir = None
 ):
 
 	start_dt = dt.strptime(start_dt_str,'%m-%d-%y')
@@ -35,57 +31,25 @@ def get_tmp_plots(
 	if not outdir:
 		outdir = askdirectory(title = 'Directory to output charts/tables to')
 
-	if feeding_paths:
+	# Get feeding data
+	feeding_dat = hmi.get_data(['FT305'],['TOTAL'],[1])['FT305_TOTAL_1hour']
+	feeding_dat['FT305'] = feeding_dat['Value']
 
-		feeding_dat	= cat.cat_dfs(
-			feeding_paths, 
-			idx_var = 'Time', 
-			output = True, 
-			output_dsn = 'HMITMP_{0}_{1}_{2}.csv'.format('AIT302', start_dt_str, end_dt_str)
-		)
-		feeding_dat['Time'] = pd.to_datetime(feeding_dat['Time'])
-	else:
-		hmi_wtr = hmi.hmi_data_agg('raw','water')
-		feeding_dat = hmi_wtr.run_report(
-			1/12,
-			['FT305'],
-			['average'],
-			start_dt_str,
-			end_dt_str,
-			hmi_path = hmi_path,
-			output_csv = 1
-		)
-
-	if tmp_paths:
-		tmp_dat = cat.cat_dfs(
-			tmp_paths, 
-			idx_var = 'Time', 
-			output = True,
-			output_dsn = 'HMIWATER_{0}_{1}_{2}.csv'.format('FT305', start_dt_str, end_dt_str)
-
-		)
-		tmp_dat['Time'] = pd.to_datetime(tmp_dat['Time'])
-	else:
-		hmi_tmp = hmi.hmi_data_agg('raw','tmp')
-		tmp_dat = hmi_tmp.run_report(
-			1/12,
-			['AIT302'],
-			['average'],
-			start_dt_str,
-			end_dt_str,
-			hmi_path = hmi_path,
-			output_csv = 1
-		)
-
+	tmp_dat = hmi.get_data(['AIT302'],['AVERAGE'],[1])['AIT302_AVERAGE_1hour']
+	tmp_dat['AIT302'] = tmp_dat['Value']
+	os.chdir(outdir)
+	tmp_dat.to_csv('tmp.csv')
+	feeding_dat.to_csv('feeding.csv')
+	# Merge the two files   
 	tmp_feed_dat = feeding_dat.merge(tmp_dat, on = 'Time')
+
 	# Remove index!
 	tmp_feed_dat.reset_index(inplace = True)
 
 	# Group the dataset into days and weeks
+	tmp_feed_dat['Time'] = pd.to_datetime(tmp_feed_dat['Time'])
 	tmp_feed_dat['Week'] = tmp_feed_dat['Time'].dt.week
-	# tmp_feed_dat['Week'] = 
 	tmp_feed_dat['tel_day'] = tmp_feed_dat['Time'].dt.hour*60 + tmp_feed_dat['Time'].dt.minute
-
 	tmp_feed_dat['Day']  = tmp_feed_dat['Time'].dt.weekday
 	tmp_feed_dat['Hour'] = tmp_feed_dat['Time'].dt.hour + tmp_feed_dat['Time'].dt.weekday*24
 
@@ -104,7 +68,7 @@ def get_tmp_plots(
 	# Last two months (or entire date range)
 	# TMP
 	ax1 = plt.subplot2grid((16,1),(0,0), rowspan = 2)
-	ax1.plot(tmp_feed_dat['Time'],tmp_feed_dat['AIT302_AVERAGE'], 'g-', linewidth = 0.5)
+	ax1.plot(tmp_feed_dat['Time'],tmp_feed_dat['AIT302'], 'g-', linewidth = 0.5)
 	ax1.set_title(
 		'Hourly Average TMP and Permeate Flux ({0} to {1})'.format(start_dt_str, end_dt_str),
 		fontweight = 'bold'
@@ -113,14 +77,14 @@ def get_tmp_plots(
 	ax1.xaxis.set_ticklabels([])
 	# Flow
 	ax2 = plt.subplot2grid((16,1),(2,0), rowspan = 2)
-	ax2.plot(tmp_feed_dat['Time'],tmp_feed_dat['FT305_AVERAGE'], 'b-', linewidth = 0.5)
+	ax2.plot(tmp_feed_dat['Time'],tmp_feed_dat['FT305']/60, 'b-', linewidth = 0.5)
 	ax2.set_ylabel('Flow (gpm)')
 	labels = ax2.get_xticklabels()
 	plt.setp(labels, rotation=45, fontsize=10)
 	# Last week
 	# TMP
 	ax3 = plt.subplot2grid((16,1),(6,0), rowspan = 2)
-	ax3.plot(tmp_feed_week['Time'],tmp_feed_week['AIT302_AVERAGE'], 'g-', linewidth = 0.5)
+	ax3.plot(tmp_feed_week['Time'],tmp_feed_week['AIT302'], 'g-', linewidth = 0.5)
 	ax3.set_title(
 		'Hourly Average TMP and Permeate Flux (last 7 days)',
 		fontweight = 'bold'
@@ -129,14 +93,14 @@ def get_tmp_plots(
 	ax3.xaxis.set_ticklabels([])
 	# Flow
 	ax4 = plt.subplot2grid((16,1),(8,0), rowspan = 2)
-	ax4.plot(tmp_feed_week['Time'],tmp_feed_week['FT305_AVERAGE'], 'b-', linewidth = 0.5)
+	ax4.plot(tmp_feed_week['Time'],tmp_feed_week['FT305']/60, 'b-', linewidth = 0.5)
 	ax4.set_ylabel('Flow (gpm)')
 	labels = ax4.get_xticklabels()
 	plt.setp(labels, rotation=45, fontsize=10)
 	# Last day
 	# TMP
 	ax5 = plt.subplot2grid((16,1),(12,0), rowspan = 2)
-	ax5.plot(tmp_feed_day['Time'],tmp_feed_day['AIT302_AVERAGE'], 'g-', linewidth = 0.5)
+	ax5.plot(tmp_feed_day['Time'],tmp_feed_day['AIT302'], 'g-', linewidth = 0.5)
 	ax5.set_title(
 		'Hourly Average TMP and Permeate Flux (last 24 hours)',
 		fontweight = 'bold'
@@ -145,7 +109,7 @@ def get_tmp_plots(
 	ax5.xaxis.set_ticklabels([])
 	# Flow
 	ax6 = plt.subplot2grid((16,1),(14,0), rowspan = 2)
-	ax6.plot(tmp_feed_day['Time'],tmp_feed_day['FT305_AVERAGE'], 'b-', linewidth = 0.5)
+	ax6.plot(tmp_feed_day['Time'],tmp_feed_day['FT305']/60, 'b-', linewidth = 0.5)
 	ax6.set_ylabel('Flow (gpm)')
 	labels = ax6.get_xticklabels()
 	plt.setp(labels, rotation=45, fontsize=10)
@@ -159,31 +123,6 @@ def get_tmp_plots(
 		width = 20, 
 		height = 160
 	)
-	
-
-	# Plot hourly tmp values at finer time scale
-	if detailed:
-		sns.set_style('white')
-		tplot = sns.FacetGrid(
-			tmp_feed_dat,
-			col = 'Week',
-			hue = 'Day',
-			sharey = True,
-			col_wrap = 3,
-			legend_out = True
-		)
-		tplot.map(plt.plot,'tel_day','AIT302_AVERAGE', linestyle = '-', linewidth = 0.5)
-		tplot.set_xlabels('Minutes Elapsed (since 0:00)')
-		tplot.set_ylabels('Average TMP (psia)')
-		tplot.add_legend()
-
-		# Output!
-		plot_filename  = "TMP_DETAILED_{0}_{1}.png".format(start_dt_str, end_dt_str)
-		plt.savefig(
-			os.path.join(outdir, plot_filename), 
-			width = 20, 
-			height = 160
-		)
 
 
 def get_feed_sumst(
@@ -192,8 +131,6 @@ def get_feed_sumst(
 	start_dt_str,
 	end_dt_str,
 	outdir = None,
-	hmi_path = None,
-	feeding_paths = None,
 	sum_period = 'DAY', 
 	plt_type = None, 
 	plt_colors = None,
@@ -224,23 +161,11 @@ def get_feed_sumst(
 	if not outdir:
 		outdir = askdirectory(title = 'Directory to output charts/tables to')
 
-	if feeding_paths:
-		feeding_dat	= cat.cat_dfs(
-			feeding_paths, 
-			idx_var = 'Time'
-		)
-		feeding_dat['Time'] = pd.to_datetime(feeding_dat['Time'])
-	else:
-		hmi_agg = hmi.hmi_data_agg('raw',stype)
-		feeding_dat = hmi_agg.run_report(
-			24,
-			elids,
-			['total','total'],
-			start_dt_str,
-			end_dt_str,
-			hmi_path = hmi_path,
-			output_csv = 1
-		)
+	feeding_dat_all = hmi.get_data(elids,['TOTAL','TOTAL'], [1,1])
+	feeding_dat = feeding_dat_all['{0}_{1}_{2}hour'.format(elids[0],'TOTAL',1)]
+	for elid in elids:
+		feeding_dat[elid] = feeding_dat_all['{0}_{1}_{2}hour'.format(elid,'TOTAL',1)]['Value']
+
 
 	# Retrieve element ids from aggregated data
 	all_elids = '_'.join(elids)
@@ -296,11 +221,10 @@ def get_feed_sumst(
 			tic_vals = ['{0} - {1}'.format(int(tic_val), int(tic_val + 1)) for tic_val in tic_vals]
 
 		if plt_type == 'BAR':
-			varnames = [elid + '_TOTAL' for elid in elids]
-			ax = agg_sumst[varnames].plot.bar(stacked = False, width = 0.8, color = plt_colors)
+			ax = agg_sumst[elids].plot.bar(stacked = False, width = 0.8, color = plt_colors)
 			plt.xticks(tic_idxs,tic_vals)
 		else:
-			ax = agg_sumst[varnames].plot(color = plt_colors)
+			ax = agg_sumst[elids].plot(color = plt_colors)
 
 		plt.ylabel(ylabel)
 		plt.legend()
@@ -331,36 +255,15 @@ def get_feed_sumst(
 		)
 
 
-# get_tmp_plots(
-# 	'8-21-17',
-# 	'10-21-17',
-# 	outdir = 'C:/Users/jbolorinos/Google Drive/Codiga Center/Charts and Data/Monitoring Reports/10-21-17',
-# 	detailed = 1,
-# 	hmi_path = 'C:/Users/jbolorinos/Google Drive/Codiga Center/HMI Data/Reactor Feeding - Raw_20171022073204.csv',
-# 	feeding_paths = [
-# 		'C:/Users/jbolorinos/Google Drive/Codiga Center/HMI Data/HMIWATER0.08_FT305_08-19-17_10-01-17.csv',
-# 		'C:/Users/jbolorinos/Google Drive/Codiga Center/HMI Data/HMIWATER0.08_FT305_10-01-17_10-13-17.csv',
-# 		'C:/Users/jbolorinos/Google Drive/Codiga Center/HMI Data/HMIWATER0.08_FT305_10-13-17_10-21-17.csv'
-# 	],
-# 	tmp_paths = [
-# 		'C:/Users/jbolorinos/Google Drive/Codiga Center/HMI Data/HMITMP0.08_AIT302_08-13-17_10-13-17.csv',
-# 		'C:/Users/jbolorinos/Google Drive/Codiga Center/HMI Data/HMITMP0.08_AIT302_10-10-17_10-21-17.csv'
-# 	]
-# )
-
-
+get_tmp_plots(
+	'8-21-17',
+	'10-28-17'
+)
 get_feed_sumst(
 	'gas',
 	['plot','table'],
 	'8-21-17',
-	'10-21-17',
-	outdir = 'C:/Users/jbolorinos/Google Drive/Codiga Center/Charts and Data/Monitoring Reports/10-21-17',
-	# hmi_path = 'C:/Users/jbolorinos/Google Drive/Codiga Center/HMI Data/Reactor Feeding - Raw_20171022073204.csv',
-	feeding_paths = [
-		'C:/Users/jbolorinos/Google Drive/Codiga Center/HMI Data/HMIGAS_FT700_FT704_10-01-17_10-13-17.csv',
-		'C:/Users/jbolorinos/Google Drive/Codiga Center/HMI Data/HMIGAS_FT700_FT704_08-13-17_10-01-17.csv',
-		'C:/Users/jbolorinos/Google Drive/Codiga Center/HMI Data/HMIGAS1_FT700_FT704_10-10-17_10-21-17.csv'
-	],
+	'10-28-17',
 	sum_period = 'DAY', 
 	plt_type = 'bar', 
 	plt_colors = ['#90775a','#eeae10'],
@@ -370,14 +273,7 @@ get_feed_sumst(
 	'water',
 	['plot','table'],
 	'8-21-17',
-	'10-21-17',
-	outdir = 'C:/Users/jbolorinos/Google Drive/Codiga Center/Charts and Data/Monitoring Reports/10-21-17',
-	hmi_path = 'C:/Users/jbolorinos/Google Drive/Codiga Center/HMI Data/Reactor Feeding - Raw_20171022073204.csv',
-	feeding_paths = [
-		'C:/Users/jbolorinos/Google Drive/Codiga Center/HMI Data/HMIWATER_FT202_FT305_10-01-17_10-13-17.csv',
-		'C:/Users/jbolorinos/Google Drive/Codiga Center/HMI Data/HMIWATER_FT202_FT305_08-13-17_10-01-17.csv',
-		'C:/Users/jbolorinos/Google Drive/Codiga Center/HMI Data/HMIWATER24_FT202_FT305_10-13-17_10-21-17.csv'
-	],
+	'10-28-17',
 	sum_period = 'DAY', 
 	plt_type = 'bar', 
 	plt_colors = ['#8c9c81','#7fbfff'],
