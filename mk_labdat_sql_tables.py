0bba0cd7951b176db3b5e5d7bd1bc1629107352f
jbolorinos <josebolorinos@gmail.com>
2017-10-29 18:26:42 -0700

    Cleaned up mk_labdat_sql_tables.py script

--- mk_labdat_sql_tables.py
+++ mk_labdat_sql_tables.py
@@ -3,66 +3,58 @@
 	(NOTE: will fail if a table already exists in the directory)
 '''
 
-import pandas as pd
 import os
 import sqlite3
 import get_lab_data as gld
 
 
-# Get directory with lab data
-data_indir = gld.get_indir()
+def mk_sql_table(table_dir, mtype):
 
-# Open SQL connection in data directory
-os.chdir(data_indir)
-conn = sqlite3.connect('cr2c_lab_data.db')
-curs = conn.cursor()
+	if mtype in ['COD','TSS','VSS','TSS_VSS','VFA']:
 
-# SQL commands for creating tables
-mk_COD_table_sql     = """
-	CREATE TABLE COD_data(
-		Date_Time TEXT, Stage TEXT, Type TEXT, obs_id TEXT, Value REAL,
-		unique(Date_Time, Stage, Type, obs_id, Value)
-	)
-"""
-mk_TSS_VSS_table_sql = """
-	CREATE TABLE TSS_VSS_data(
-		id INTEGER PRIMARY KEY, Date_Time TEXT, Stage TEXT, Type TEXT, obs_id TEXT, Value REAL,
-		unique(Date_Time, Stage, Type, obs_id, Value)
-	)
-"""
-mk_ALK_table_sql     = """
-	CREATE TABLE ALK_data(
-		id INTEGER PRIMARY KEY, Date_Time TEXT, Stage TEXT, obs_id TEXT, Value REAL,
-		unique(Date_Time, Stage, obs_id, Value)
-	)
-"""
-mk_PH_table_sql      = """
-	CREATE TABLE PH_data(
-		id INTEGER PRIMARY KEY, Date_Time TEXT, Stage TEXT, obs_id TEXT, Value REAL,
-		unique(Date_Time, Stage, obs_id, Value)
-	)
-"""
-mk_VFA_table_sql     = """
-	CREATE TABLE VFA_data(
-		id INTEGER PRIMARY KEY, Date_Time TEXT, Stage TEXT, Type TEXT, obs_id TEXT, Value REAL,
-		unique(Date_Time, Stage, Type, obs_id, Value)
-	)
-"""
-mk_GasComp_table_sql     = """
-	CREATE TABLE GasComp_data(
-		id INTEGER PRIMARY KEY, Date_Time TEXT, Hel_Pressure REAL, Type TEXT, obs_id TEXT, Value REAL,
-		unique(Date_Time, Hel_Pressure, Type, obs_id, Value)
-	)
-"""
+		mk_sql_str = """
+			CREATE TABLE {}_data(
+				id INTEGER PRIMARY KEY Date_Time TEXT, Stage TEXT, Type TEXT, obs_id TEXT, Value REAL,
+				unique(Date_Time, Stage, Type, obs_id, Value)
+			)
+		""".format(mtype)
 
-# Execute SQL commands
-# curs.execute(mk_COD_table_sql)
-# curs.execute(mk_TSS_VSS_table_sql)
-# curs.execute(mk_ALK_table_sql)
-# curs.execute(mk_PH_table_sql)
-# curs.execute(mk_VFA_table_sql)
-curs.execute(mk_GasComp_table_sql)
+	elif mtype in ['PH','ALKALINITY']:
 
-# Close connection
-conn.close()
+		mk_sql_str = """
+			CREATE TABLE {}_data(
+				id INTEGER PRIMARY KEY, Date_Time TEXT, Stage TEXT, obs_id TEXT, Value REAL,
+				unique(Date_Time, Stage, obs_id, Value)
+			)
+		""".format(mtype)
+
+	elif mtype == 'GasComp':
+
+		mk_sql_str = """
+			CREATE TABLE GasComp_data(
+				id INTEGER PRIMARY KEY, Date_Time TEXT, Hel_Pressure REAL, Type TEXT, obs_id TEXT, Value REAL,
+				unique(Date_Time, Hel_Pressure, Type, obs_id, Value)
+			)
+		"""
+
+	# Open SQL connection in data directory
+	os.chdir(data_indir)
+	conn = sqlite3.connect('cr2c_lab_data.db')
+	curs = conn.cursor()
+
+	# Execute the SQL command to create the table
+	curs.execute(mk_sql_str)
+	# Close connection
+	conn.close()
+
+
+if __name__ == '__main__':
+
+	# Get directory with lab data
+	table_dir = gld.get_indir()
+
+	mtypes = ['COD','VFA','TSS_VSS','PH','ALKALINITY','GasComp']
+	
+	for mtype in mtypes:
+		mk_sql_table(table_dir, mtype)
 
