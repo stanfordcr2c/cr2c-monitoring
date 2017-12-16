e6423c24d0569b5b3463a83ed69eada8824f956d
jbolorinos <josebolorinos@gmail.com>
2017-12-15 22:03:32 -0800

    Modified get_cod_balance to use moving averages and less resolved time calculations

--- get_cod_balance.py
+++ get_cod_balance.py
@@ -15,6 +15,7 @@ import scipy as sp
 from scipy import interpolate as ip
 import pandas as pd
 import datetime as datetime
+from datetime import timedelta
 from datetime import datetime as dt
 from pandas import read_excel
 import os
@@ -23,7 +24,7 @@ import functools
 from tkinter.filedialog import askopenfilename
 from tkinter.filedialog import asksaveasfilename
 from tkinter.filedialog import askdirectory
-import get_lab_data as gld
+import process_lab_data as pld
 import hmi_data_agg as hmi
 from hmi_data_agg import hmi_data_agg as hmi_run
 import est_biogas_prod as ebg
@@ -49,7 +50,9 @@ def get_cod_bal(
 		tperiod_hrs = tperiod
 
 	if not outdir:
-		outdir = askdirectory(title = 'Directory to output summary statistics/plots to')
+		tkTitle = 'Directory to output summary statistics/plots to'
+		print(tkTitle)
+		outdir = askdirectory(title = tkTitle)
 
 	gas_elids  = ['FT700','FT704']
 	temp_elids = ['AT304','AT310']
@@ -61,8 +64,12 @@ def get_cod_bal(
 	afmbr_vol = 1700 # in L
 	l_p_gal = 3.78541 # Liters/Gallon
 
+
+	#=========================================> HMI DATA <=========================================
+	
 	# If requested, run the hmi_data_agg script for the reactor meters and time period of interest
-	get_hmi = hmi_run(start_dt_str, end_dt_str)
+	if run_agg_feeding or run_agg_gasprod or run_agg_temp:
+		get_hmi = hmi_run(start_dt_str, end_dt_str)
 	if run_agg_feeding:
 		get_hmi.run_report(
 			[tperiod]*2, # Number of hours you want to average over
@@ -85,9 +92,10 @@ def get_cod_bal(
 			['temp']*len(temp_elids), # Type of sensor (case insensitive, can be water, gas, pH, conductivity, temp, or tmp
 		)
 
-	# Read in the data (data requested in lines above is now in sql file and can be queried)
+	# Read in the data
 	year = start_dt.year
-	gasprod_all = hmi.get_data(
+
+	gasprod_dat = hmi.get_data(
 		gas_elids,
 		[tperiod]*len(gas_elids),
 		[ttype]*len(gas_elids), 
@@ -95,13 +103,8 @@ def get_cod_bal(
 		start_dt_str = start_dt_str, 
 		end_dt_str = end_dt_str
 	)
-	gasprod_dat = gasprod_all['{0}_{1}{2}_AVERAGES'.format(gas_elids[0],tperiod,ttype)]
-	# Rename the "Value" variables to the relevant element ids (elids)
-	for elid in gas_elids:
-		gasprod_dat[elid] = gasprod_all['{0}_{1}{2}_AVERAGES'.format(elid,tperiod,ttype)]['Value']
-
 	# Do the same for feeding and temperature
-	feeding_all = hmi.get_data(
+	feeding_dat = hmi.get_data(
 		[inf_elid, eff_elid],
 		[tperiod]*2, 
 		[ttype]*2, 
@@ -109,30 +112,53 @@ def get_cod_bal(
 		start_dt_str = start_dt_str,
 		end_dt_str = end_dt_str
 	)
-	feeding_dat = feeding_all['{0}_{1}{2}_AVERAGES'.format(inf_elid,tperiod,ttype)]
-	feeding_dat[inf_elid] = feeding_dat['Value']*l_p_gal
-	feeding_dat[eff_elid] = feeding_all['{0}_{1}{2}_AVERAGES'.format(eff_elid,tperiod,ttype)]['Value']*l_p_gal
-	temp_all = hmi.get_data(
+	temp_dat = hmi.get_data(
 		temp_elids,
 		[tperiod]*len(temp_elids), 
 		[ttype]*len(temp_elids), 
 		year = year,
 		start_dt_str = start_dt_str,
 		end_dt_str = end_dt_str
-	)
-	temp_dat = temp_all['{0}_{1}{2}_AVERAGES'.format(temp_elids[0],tperiod,ttype)]
-	for elid in temp_elids:
-		temp_dat[elid] = temp_all['{0}_{1}{2}_AVERAGES'.format(elid,tperiod,ttype)]['Value'] 
+	) 
 
+	# Prep the HMI data
+	# NOTE 1: # Getting totals as the average flow (in liters or gallons per minute) x 
+	# 60 x 
+	# time period in hour equivalents x
+	# size of the time period
+	# NOTE 2: for now, getting the date of the time step to merge onto daily data
+	# In the future we could linearly interpolate between two different values on two days...
+	gasprod_dat['Meas Biogas Prod'] = (gasprod_dat['FT700'] + gasprod_dat['FT704'])*60*tperiod_hrs*tperiod
+	gasprod_dat_cln                 = gasprod_dat[['Time','Meas Biogas Prod']]
+
+	# Feeding HMI Data
+	feeding_dat['Flow In']  = feeding_dat[inf_elid]*60*tperiod_hrs*tperiod*l_p_gal
+	feeding_dat['Flow Out'] = feeding_dat[eff_elid]*60*tperiod_hrs*tperiod*l_p_gal
+	feeding_dat_cln         = feeding_dat[['Time','Flow In','Flow Out']]
+
+	# Reactor Temperature HMI data
+	temp_dat['Reactor Temp'] = temp_dat[temp_elids].mean(axis = 1)
+	temp_dat_cln             = temp_dat[['Time','Reactor Temp']]
+
+	# List of hmi dataframes
+	hmi_dflist = [temp_dat_cln, feeding_dat_cln, gasprod_dat_cln]
+	# Merge hmi datasets
+	hmidat = functools.reduce(lambda left,right: pd.merge(left,right, on='Time', how = 'outer'), hmi_dflist)
+	hmidat['Date'] = hmidat['Time'].dt.date
+	
+	#=========================================> HMI DATA <=========================================
+
+	#=========================================> LAB DATA <=========================================
+	
 	# Get lab data from file on box and filter to desired dates
-	labdat  = gld.get_data(['COD','GasComp'])
+	labdat  = pld.labrun().get_data(['COD','GasComp'])
 	
 	# COD data
 	cod_dat = labdat['COD']
 	cod_dat['Date_Time'] = pd.to_datetime(cod_dat['Date_Time'])
 	cod_dat = cod_dat.loc[
 		(cod_dat['Date_Time'] >= start_dt) &
-		(cod_dat['Date_Time'] <= end_dt)
+		(cod_dat['Date_Time'] <= end_dt + timedelta(days = 1))
 	]
 	# Drop duplicates
 	cod_dat.drop_duplicates(keep = 'first', inplace = True)
@@ -182,59 +208,48 @@ def get_cod_bal(
 	gc_dat_cln = gc_dat_wide[['Time','CH4%','CO2%']]
 	gc_dat_cln.columns = ['Time','CH4%','CO2%']
 
-	# Prep the HMI data
-	# NOTE 1: # Getting totals as the average flow (in liters or gallons per minute) x 
-	# 60 x 
-	# time period in hour equivalents x
-	# size of the time period
-	# NOTE 2: for now, getting the date of the time step to merge onto daily data
-	# In the future we could linearly interpolate between two different values on two days...
-	gasprod_dat['Time']       = pd.to_datetime(gasprod_dat['Time'])
-	gasprod_dat['Meas Biogas Prod'] = (gasprod_dat['FT700'] + gasprod_dat['FT704'])*60*tperiod_hrs*tperiod
-	gasprod_dat_cln           = gasprod_dat[['Time','Meas Biogas Prod']]
-
-	# Feeding HMI Data
-	feeding_dat['Time']     = pd.to_datetime(feeding_dat['Time'])
-	feeding_dat['Flow In']  = feeding_dat[inf_elid]*60*tperiod_hrs*tperiod
-	feeding_dat['Flow Out'] = feeding_dat[eff_elid]*60*tperiod_hrs*tperiod
-	feeding_dat_cln         = feeding_dat[['Time','Flow In','Flow Out']]
-
-	# Reactor Temperature HMI data
-	temp_dat['Time']         = pd.to_datetime(temp_dat['Time'])
-	temp_dat['Reactor Temp'] = temp_dat[temp_elids].mean(axis = 1)
-	temp_dat_cln             = temp_dat[['Time','Reactor Temp']]
-
-	# List of hmi dataframes
-	hmi_dflist = [temp_dat_cln, feeding_dat_cln, gasprod_dat_cln]
-	# Merge hmi datasets
-	hmidat = functools.reduce(lambda left,right: pd.merge(left,right, on='Time', how = 'outer'), hmi_dflist)
-	hmidat['Date'] = hmidat['Time'].dt.date
-	hmidat.rename(columns = {'Time': 'HMITime'}, inplace = True)
 	# Merge lab data by time
 	labdat = cod_dat_cln.merge(gc_dat_cln, on = 'Time', how = 'outer')
 	labdat['Date'] = labdat['Time'].dt.date
-	labdat.rename(columns = {'Time': 'LabTime'}, inplace = True)
-	# Merge all data
-	cod_bal_dat = labdat.merge(hmidat, on = 'Date', how = 'outer')
+	# Get daily average of readings if multiple readings in a day (also prevents merging issues!)
+	labdat_ud = labdat.groupby('Date').mean()
+	labdat_ud.reset_index(inplace = True)
+
+	#=========================================> LAB DATA <=========================================
+
+	#=======================================> MERGE & PREP <=======================================	
+	
+	# Merge Lab and HMI data
+	cod_bal_dat = labdat_ud.merge(hmidat, on = 'Date', how = 'outer')
+
 	# Dedupe (merging many files, so any duplicates will cause big problems!)
-	cod_bal_dat.drop_duplicates(inplace = True)
+	# cod_bal_dat.drop_duplicates(inplace = True)
 
-	# Offset time (after sampling each day, so it is associated wih the next sampling day) 
-	cod_bal_dat.loc[cod_bal_dat['HMITime'] > cod_bal_dat['LabTime'],'Date'] = cod_bal_dat['Date'] + datetime.timedelta(days = 1)
-
-
+	# Calculate daily totals and daily means for each date
 	dly_tots  = cod_bal_dat[['Date','Flow In','Flow Out','Meas Biogas Prod']].groupby('Date').sum()
 	dly_tots.reset_index(inplace = True)
-	dly_means = cod_bal_dat[['Date','LabTime','Reactor Temp','CODs MS','CODp MS','CODs R','CODp R','CODs Out','CH4%','CO2%']].groupby('Date').mean()
+	dly_means = cod_bal_dat[['Date','Reactor Temp','CODs MS','CODp MS','CODs R','CODp R','CODs Out','CH4%','CO2%']].groupby('Date').mean()
 	dly_means.reset_index(inplace = True)
 
+	# Merge and fill in missing values
 	cod_bal_dly = dly_tots.merge(dly_means, on = 'Date', how = 'outer')
 	cod_bal_dly.set_index('Date')
 	cod_bal_dly[['CH4%','CO2%','CODs MS','CODp MS','CODs R','CODp R','CODs Out']] = \
 		cod_bal_dly[['CH4%','CO2%','CODs MS','CODp MS','CODs R','CODp R','CODs Out']].interpolate()
-	cod_bal_dly.reset_index(inplace = True)
 
-	# ============> Estimating COD Consumption <============
+	# Get moving average of COD in reactors (data bounce around a lot)
+	ma_win = 14
+	cod_bal_dly['CODp R'] = cod_bal_dly['CODp R'].rolling(ma_win).mean()
+	cod_bal_dly['CODs R'] = cod_bal_dly['CODs R'].rolling(ma_win).mean()
+	cod_bal_dly['CODp MS'] = cod_bal_dly['CODp MS'].rolling(ma_win).mean()
+	cod_bal_dly['CODs MS'] = cod_bal_dly['CODs MS'].rolling(ma_win).mean()
+	cod_bal_dly['CODs Out'] = cod_bal_dly['CODs Out'].rolling(ma_win).mean()
+
+	cod_bal_dly.reset_index(inplace = True)
+	
+	#=======================================> MERGE & PREP <=======================================	
+
+	#=================================> Estimate COD Consumption <=================================	
 
 	# First estimate particulate COD hydrolized by comparing the particulate COD
 	# that should accumulate in the reactor from influent particulate COD vs actual particulate COD
@@ -247,10 +262,12 @@ def get_cod_bal(
 		)/\
 		(afbr_vol + afmbr_vol)
 	# The hydrolized COD is the difference between the accumulated vs observed particulate COD
-	cod_bal_dly['CODp R hyd'] = cod_bal_dly['CODp R pot'] - cod_bal_dly['CODp R']
+	cod_bal_dly.loc[:,'CODp R hyd'] = cod_bal_dly['CODp R pot'].values - cod_bal_dly['CODp R'].values
+	# Replace negative values with zero (no observable hydrolysis)
+	cod_bal_dly.loc[cod_bal_dly['CODp R hyd'] < 0,'CODp R hyd'] = 0
 
 	# Next compute the soluble COD that would accumulate without consumption by the biology
-	cod_bal_dly['CODs R pot'] = \
+	cod_bal_dly.loc[:,'CODs R pot'] = \
 		(
 			# Mass that was in the reactors in the prior timestep
 			cod_bal_dly['CODs R'].shift(1)*(afbr_vol + afmbr_vol) +
@@ -262,12 +279,17 @@ def get_cod_bal(
 			cod_bal_dly['CODs Out']*cod_bal_dly['Flow Out'].shift(1)
 		)/\
 		(afbr_vol + afmbr_vol)
-	# Consumed COD is the difference between the accumulated vs observed soluble COD
-	cod_bal_dly['COD Consumed'] = cod_bal_dly['CODs R pot'] - cod_bal_dly['CODs R']
+	# Consumed COD is the difference between the accumulated vs observed soluble COD (dividing by 1000 to get kg per m^3)
+	cod_bal_dly.loc[:,'COD Consumed'] = \
+		(cod_bal_dly['CODs R pot'] - cod_bal_dly['CODs R'])*(afbr_vol + afmbr_vol)/1000
+	# Replace negative values with zero (no observable COD consumption)
+	cod_bal_dly.loc[cod_bal_dly['COD Consumed'] < 0, 'COD Consumed'] = 0
 
-	# ============> Estimating COD Consumption <============
+	#=================================> Estimate COD Consumption <=================================	
 
-	# # Get theoretical estimated methane output
+	#========================================> COD Balance <=======================================	
+	
+	# Get theoretical estimated methane output
 	gasprod_thry = []
 	for index,row in cod_bal_dly.iterrows():
 		gasprod_thry.append(
@@ -277,7 +299,7 @@ def get_cod_bal(
 				temp = row['Reactor Temp'], 
 				percCH4 = row['CH4%']/100, 
 				percCO2 = row['CO2%']/100, 
-				flowrate = row['Flow In']/1000, 
+				flowrate = 1, 
 				precision = 1E-6
 			)
 		)
@@ -287,16 +309,16 @@ def get_cod_bal(
 	# Actual estimated CH4 production
 	cod_bal_dly['Meas CH4 Prod'] = cod_bal_dly['Meas Biogas Prod']*cod_bal_dly['CH4%']/100
 	cod_bal_dly['Biogas Discrep (%)'] =	(cod_bal_dly['Meas Biogas Prod']/cod_bal_dly['Thr Biogas Prod'] - 1)*100
-	cod_bal_dly.loc[np.isnan(cod_bal_dly['Biogas Discrep (%)']),'Biogas Discrep (%)'] = 100
 	cod_bal_dly.loc[cod_bal_dly['Biogas Discrep (%)'] > 100,'Biogas Discrep (%)'] = 100
 	cod_bal_dly['CH4 Discrep (%)']    =	(cod_bal_dly['Meas CH4 Prod']/cod_bal_dly['Thr CH4 Prod'] - 1)*100
-	cod_bal_dly.loc[np.isnan(cod_bal_dly['CH4 Discrep (%)']),'CH4 Discrep (%)'] = 100
 	cod_bal_dly.loc[cod_bal_dly['CH4 Discrep (%)'] > 100,'CH4 Discrep (%)'] = 100
+
+	#========================================> COD Balance <=======================================	
 
 	# Output csv with summary statistics
 	os.chdir(outdir)
 	output_vars = ['Date','Meas Biogas Prod','Thr Biogas Prod','Meas CH4 Prod','Thr CH4 Prod','Biogas Discrep (%)','CH4 Discrep (%)']
-	cod_bal_dly[output_vars].to_csv('COD Balance.csv')
+	cod_bal_dly.to_csv('COD Balance Full.csv')
 	days_el = (cod_bal_dly['Date'] - cod_bal_dly['Date'][0])/np.timedelta64(24,'h')
 
 	cod_bal_dly.rename(
@@ -325,14 +347,13 @@ def get_cod_bal(
 	)
 
 
-
 get_cod_bal(
 	'8-12-17',
-	'11-1-17',
+	'12-10-17',
 	1,
 	'hour',
-	# run_agg_temp = True,
-	outdir = '/Users/josebolorinos/Google Drive/Codiga Center/Miscellany',
+	outdir = '/Users/josebolorinos/Google Drive/Codiga Center/Miscellany'
+	# run_agg_temp = True 
 
 	
 )
