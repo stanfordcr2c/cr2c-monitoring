b50cbf7484503947b1a7769004893eeaaadbb655
jbolorinos <josebolorinos@gmail.com>
2018-11-30 17:40:17 -0800

    Added basic callback functionality to main.py

--- main.py
+++ main.py
@@ -41,8 +41,18 @@ app.config['suppress_callback_exceptions'] = True
 app.css.config.serve_locally = True
 app.scripts.config.serve_locally = True
 
- 
-app.layout = html.Div(id = 'page-content', children = ['Hello CR2C!'])
+app.layout = html.Div([
+    dcc.Input(id='my-id', value='initial value', type='text'),
+    html.Div(id='my-div'),
+    dcc.Graph(id = 'go')
+])
+
+@app.callback(
+    Output(component_id='my-div', component_property='children'),
+    [Input(component_id='my-id', component_property='value')]
+)
+def update_output_div(input_value):
+    return 'You\'ve entered "{}"'.format(input_value)
 
 
 if __name__ == '__main__':
